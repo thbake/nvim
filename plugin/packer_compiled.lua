e27ca666df4198fd30ff159aecea60177dacc1d5
thbake <thomas.bake@hotmail.com>
2023-12-07 16:21:24 +0100

    Deleted some pdf file and recompiled packer

--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/home/thbake/.cache/nvim/packer_hererocks/2.1.1692716794/share/lua/5.1/?.lua;/home/thbake/.cache/nvim/packer_hererocks/2.1.1692716794/share/lua/5.1/?/init.lua;/home/thbake/.cache/nvim/packer_hererocks/2.1.1692716794/lib/luarocks/rocks-5.1/?.lua;/home/thbake/.cache/nvim/packer_hererocks/2.1.1692716794/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/home/thbake/.cache/nvim/packer_hererocks/2.1.1692716794/lib/lua/5.1/?.so"
+local package_path_str = "/home/tbake/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?.lua;/home/tbake/.cache/nvim/packer_hererocks/2.1.0-beta3/share/lua/5.1/?/init.lua;/home/tbake/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?.lua;/home/tbake/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/home/tbake/.cache/nvim/packer_hererocks/2.1.0-beta3/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -76,121 +76,121 @@ time([[Defining packer_plugins]], true)
 _G.packer_plugins = {
   LuaSnip = {
     loaded = true,
-    path = "/home/thbake/.local/share/nvim/site/pack/packer/start/LuaSnip",
+    path = "/home/tbake/.local/share/nvim/site/pack/packer/start/LuaSnip",
     url = "https://github.com/L3MON4D3/LuaSnip"
   },
   ["cmp-buffer"] = {
     loaded = true,
-    path = "/home/thbake/.local/share/nvim/site/pack/packer/start/cmp-buffer",
+    path = "/home/tbake/.local/share/nvim/site/pack/packer/start/cmp-buffer",
     url = "https://github.com/hrsh7th/cmp-buffer"
   },
   ["cmp-nvim-lsp"] = {
     loaded = true,
-    path = "/home/thbake/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
+    path = "/home/tbake/.local/share/nvim/site/pack/packer/start/cmp-nvim-lsp",
     url = "https://github.com/hrsh7th/cmp-nvim-lsp"
   },
   ["cmp-path"] = {
     loaded = true,
-    path = "/home/thbake/.local/share/nvim/site/pack/packer/start/cmp-path",
+    path = "/home/tbake/.local/share/nvim/site/pack/packer/start/cmp-path",
     url = "https://github.com/hrsh7th/cmp-path"
   },
   cmp_luasnip = {
     loaded = true,
-    path = "/home/thbake/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
+    path = "/home/tbake/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
   everforest = {
     loaded = true,
-    path = "/home/thbake/.local/share/nvim/site/pack/packer/start/everforest",
+    path = "/home/tbake/.local/share/nvim/site/pack/packer/start/everforest",
     url = "https://github.com/sainnhe/everforest"
   },
   ["gruvbox.nvim"] = {
     loaded = true,
-    path = "/home/thbake/.local/share/nvim/site/pack/packer/start/gruvbox.nvim",
+    path = "/home/tbake/.local/share/nvim/site/pack/packer/start/gruvbox.nvim",
     url = "https://github.com/ellisonleao/gruvbox.nvim"
   },
   harpoon = {
     loaded = true,
-    path = "/home/thbake/.local/share/nvim/site/pack/packer/start/harpoon",
+    path = "/home/tbake/.local/share/nvim/site/pack/packer/start/harpoon",
     url = "https://github.com/ThePrimeagen/harpoon"
   },
   ["julia-vim"] = {
     loaded = true,
-    path = "/home/thbake/.local/share/nvim/site/pack/packer/start/julia-vim",
+    path = "/home/tbake/.local/share/nvim/site/pack/packer/start/julia-vim",
     url = "https://github.com/JuliaEditorSupport/julia-vim"
   },
   ["kanagawa.nvim"] = {
     loaded = true,
-    path = "/home/thbake/.local/share/nvim/site/pack/packer/start/kanagawa.nvim",
+    path = "/home/tbake/.local/share/nvim/site/pack/packer/start/kanagawa.nvim",
     url = "https://github.com/rebelot/kanagawa.nvim"
   },
   ["lualine.nvim"] = {
     loaded = true,
-    path = "/home/thbake/.local/share/nvim/site/pack/packer/start/lualine.nvim",
+    path = "/home/tbake/.local/share/nvim/site/pack/packer/start/lualine.nvim",
     url = "https://github.com/nvim-lualine/lualine.nvim"
   },
   ["mason-lspconfig.nvim"] = {
     loaded = true,
-    path = "/home/thbake/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
+    path = "/home/tbake/.local/share/nvim/site/pack/packer/start/mason-lspconfig.nvim",
     url = "https://github.com/williamboman/mason-lspconfig.nvim"
   },
   ["mason.nvim"] = {
     loaded = true,
-    path = "/home/thbake/.local/share/nvim/site/pack/packer/start/mason.nvim",
+    path = "/home/tbake/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
   ["nvim-autopairs"] = {
     config = { "\27LJ\2\n@\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\19nvim-autopairs\frequire\0" },
     loaded = true,
-    path = "/home/thbake/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
+    path = "/home/tbake/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
     url = "https://github.com/windwp/nvim-autopairs"
   },
   ["nvim-cmp"] = {
     loaded = true,
-    path = "/home/thbake/.local/share/nvim/site/pack/packer/start/nvim-cmp",
+    path = "/home/tbake/.local/share/nvim/site/pack/packer/start/nvim-cmp",
     url = "https://github.com/hrsh7th/nvim-cmp"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
-    path = "/home/thbake/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
+    path = "/home/tbake/.local/share/nvim/site/pack/packer/start/nvim-lspconfig",
     url = "https://github.com/neovim/nvim-lspconfig"
   },
   ["nvim-treesitter"] = {
     loaded = true,
-    path = "/home/thbake/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
+    path = "/home/tbake/.local/share/nvim/site/pack/packer/start/nvim-treesitter",
     url = "https://github.com/nvim-treesitter/nvim-treesitter"
   },
   ["nvim-web-devicons"] = {
     loaded = false,
     needs_bufread = false,
-    path = "/home/thbake/.local/share/nvim/site/pack/packer/opt/nvim-web-devicons",
+    path = "/home/tbake/.local/share/nvim/site/pack/packer/opt/nvim-web-devicons",
     url = "https://github.com/kyazdani42/nvim-web-devicons"
   },
   ["packer.nvim"] = {
     loaded = true,
-    path = "/home/thbake/.local/share/nvim/site/pack/packer/start/packer.nvim",
+    path = "/home/tbake/.local/share/nvim/site/pack/packer/start/packer.nvim",
     url = "https://github.com/wbthomason/packer.nvim"
   },
   ["plenary.nvim"] = {
     loaded = true,
-    path = "/home/thbake/.local/share/nvim/site/pack/packer/start/plenary.nvim",
+    path = "/home/tbake/.local/share/nvim/site/pack/packer/start/plenary.nvim",
     url = "https://github.com/nvim-lua/plenary.nvim"
   },
   ["telescope.nvim"] = {
     loaded = true,
-    path = "/home/thbake/.local/share/nvim/site/pack/packer/start/telescope.nvim",
+    path = "/home/tbake/.local/share/nvim/site/pack/packer/start/telescope.nvim",
     url = "https://github.com/nvim-telescope/telescope.nvim"
   },
   ["tokyonight.nvim"] = {
     loaded = true,
-    path = "/home/thbake/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
+    path = "/home/tbake/.local/share/nvim/site/pack/packer/start/tokyonight.nvim",
     url = "https://github.com/folke/tokyonight.nvim"
   },
   ["typst.vim"] = {
     loaded = false,
     needs_bufread = true,
     only_cond = false,
-    path = "/home/thbake/.local/share/nvim/site/pack/packer/opt/typst.vim",
+    path = "/home/tbake/.local/share/nvim/site/pack/packer/opt/typst.vim",
     url = "https://github.com/kaarmu/typst.vim"
   }
 }
@@ -208,9 +208,9 @@ vim.cmd [[au FileType typst ++once lua require("packer.load")({'typst.vim'}, { f
 time([[Defining lazy-load filetype autocommands]], false)
 vim.cmd("augroup END")
 vim.cmd [[augroup filetypedetect]]
-time([[Sourcing ftdetect script at: /home/thbake/.local/share/nvim/site/pack/packer/opt/typst.vim/ftdetect/typst.vim]], true)
-vim.cmd [[source /home/thbake/.local/share/nvim/site/pack/packer/opt/typst.vim/ftdetect/typst.vim]]
-time([[Sourcing ftdetect script at: /home/thbake/.local/share/nvim/site/pack/packer/opt/typst.vim/ftdetect/typst.vim]], false)
+time([[Sourcing ftdetect script at: /home/tbake/.local/share/nvim/site/pack/packer/opt/typst.vim/ftdetect/typst.vim]], true)
+vim.cmd [[source /home/tbake/.local/share/nvim/site/pack/packer/opt/typst.vim/ftdetect/typst.vim]]
+time([[Sourcing ftdetect script at: /home/tbake/.local/share/nvim/site/pack/packer/opt/typst.vim/ftdetect/typst.vim]], false)
 vim.cmd("augroup END")
 
 _G._packer.inside_compile = false
